8c05c4a7f6e0bb591bbf5f08dca771852f754020
soydiego <diegofranchina@gmail.com>
2021-08-16 13:18:50 +0200

    Emit, on, layout mini-chat, send messages

--- 01-socket-server/index.js
+++ 01-socket-server/index.js
@@ -1,14 +1,26 @@
 //Servidor de Express
-const app = require("express")();
+const express = require("express");
+const app = express();
 
 //Servidor de Sockets
 const server = require("http").createServer(app);
 
 //Configuración del Socket server
 const io = require("socket.io")(server);
-io.on("connection", () => {
-	/* … */
+
+//Desplegar el directorio público
+app.use(express.static(__dirname + "/public"));
+
+io.on("connection", (socket) => {
+	// socket.emit("mensaje-bienvenida", {
+	// 	msg: "Bienvenido al server...",
+	// 	fecha: new Date(),
+	// });
+
+	socket.on("mensaje-to-server", (data) => {
+		io.emit("mensaje-from-server", data);
+	});
 });
-server.listen(8080, () =>{
-    console.log('Server corriendo en puerto 8080');
+server.listen(8080, () => {
+	console.log("Server corriendo en puerto 8080");
 });
