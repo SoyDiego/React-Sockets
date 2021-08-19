4aa8fad86b79ce05122c1e258dc2ca72269ce7fa
soydiego <diegofranchina@gmail.com>
2021-08-19 13:13:47 +0200

    Installed react router dom and defining routes

--- 03-ticket-app/ticket-app/src/pages/RouterPage.js
+++ 03-ticket-app/ticket-app/src/pages/RouterPage.js
@@ -7,37 +7,61 @@ import {
 	UploadOutlined,
 } from "@ant-design/icons";
 
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	Redirect,
+} from "react-router-dom";
+import Cola from "./Cola";
+import CrearTicket from "./CrearTicket";
+import Ingresar from "./Ingresar";
+import Escritorio from "./Escritorio";
+
 const { Sider, Content } = Layout;
 
 const RouterPage = () => {
 	return (
-		<Layout style={{ height: "100vh" }}>
-			<Sider>
-				<div className="logo" />
-				<Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-					<Menu.Item key="1" icon={<UserOutlined />}>
-						Ingresar
-					</Menu.Item>
-					<Menu.Item key="2" icon={<VideoCameraOutlined />}>
-						Cola
-					</Menu.Item>
-					<Menu.Item key="3" icon={<UploadOutlined />}>
-						Crear ticket
-					</Menu.Item>
-				</Menu>
-			</Sider>
-			<Layout className="site-layout">
-				<Content
-					className="site-layout-background"
-					style={{
-						margin: "24px 16px",
-						padding: 24,
-						minHeight: 280,
-					}}>
-					Content
-				</Content>
+		<Router>
+			<Layout style={{ height: "100vh" }}>
+				<Sider>
+					<div className="logo" />
+					<Menu
+						theme="dark"
+						mode="inline"
+						defaultSelectedKeys={["1"]}>
+						<Menu.Item key="1" icon={<UserOutlined />}>
+							<Link to="/ingresar">Ingresar</Link>
+						</Menu.Item>
+						<Menu.Item key="2" icon={<VideoCameraOutlined />}>
+							<Link to="/cola">Cola de tickets</Link>
+						</Menu.Item>
+						<Menu.Item key="3" icon={<UploadOutlined />}>
+							<Link to="/crear">Crear tickets</Link>
+						</Menu.Item>
+					</Menu>
+				</Sider>
+				<Layout className="site-layout">
+					<Content
+						className="site-layout-background"
+						style={{
+							margin: "24px 16px",
+							padding: 24,
+							minHeight: 280,
+						}}>
+						<Switch>
+							<Route path="/ingresar" component={Ingresar} />
+							<Route path="/cola" component={Cola} />
+							<Route path="/crear" component={CrearTicket} />
+							<Route path="/escritorio" component={Escritorio} />
+
+							<Redirect to="/ingresar" />
+						</Switch>
+					</Content>
+				</Layout>
 			</Layout>
-		</Layout>
+		</Router>
 	);
 };
 
