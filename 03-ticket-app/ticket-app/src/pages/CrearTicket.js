96337250a3f62dfbfaaeef4e43a48fe317c95a3d
soydiego <diegofranchina@gmail.com>
2021-08-20 11:24:08 +0200

    Layout Crear ticket done

--- 03-ticket-app/ticket-app/src/pages/CrearTicket.js
+++ 03-ticket-app/ticket-app/src/pages/CrearTicket.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Button, Col, Row, Typography } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
 const CrearTicket = () => {
-    return (
-        <div>
-            Crear ticket
-        </div>
-    )
-}
+	const { Title, Text } = Typography;
 
-export default CrearTicket
+	const nuevoTicket = () => {
+		alert("Nuevo");
+	};
+
+	return (
+		<>
+			<Row>
+				<Col span={14} offset={6} align="center">
+					<Title level={3}>
+						Presione el boton para generar un nuevo ticket
+					</Title>
+
+					<Button
+						type="primary"
+						shape="round"
+						icon={<DownloadOutlined />}
+						size="large"
+						onClick={nuevoTicket}>
+						Nuevo ticket
+					</Button>
+				</Col>
+			</Row>
+
+			<Row style={{ marginTop: 100 }}>
+				<Col span={14} offset={6} align="center">
+					<Text level={2}>Su número: </Text>
+					<br />
+					<Text type="success" style={{ fontSize: 55 }}>
+						55
+					</Text>
+				</Col>
+			</Row>
+		</>
+	);
+};
+
+export default CrearTicket;
