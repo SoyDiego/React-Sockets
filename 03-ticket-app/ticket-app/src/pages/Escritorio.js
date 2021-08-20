991b80346fbf2ac4e939234e462ee30a1dcccef2
soydiego <diegofranchina@gmail.com>
2021-08-20 11:12:52 +0200

    Layout Escritorio done

--- 03-ticket-app/ticket-app/src/pages/Escritorio.js
+++ 03-ticket-app/ticket-app/src/pages/Escritorio.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
+import { Row, Col, Typography, Button, Divider } from "antd";
+import { CloseCircleOutlined, RightOutlined } from "@ant-design/icons";
 
 const Escritorio = () => {
-    return (
-        <div>
-            Escritorio Page
-        </div>
-    )
-}
+	const { Title, Text } = Typography;
 
-export default Escritorio
+	const salir = () => {
+		alert("salir");
+	};
+
+	const siguienteTicket = () => {
+		alert("Siguiente");
+	};
+
+	return (
+		<>
+			<Row>
+				<Col span={20}>
+					<Title level={2}>Diego</Title>
+					<Text>Usted está trabajando en el escritorio: </Text>
+					<Text type="success">5</Text>
+				</Col>
+
+				<Col span={4} align="right">
+					<Button shape="round" type="danger" onClick={salir}>
+						<CloseCircleOutlined /> Salir
+					</Button>
+				</Col>
+			</Row>
+
+			<Divider />
+
+			<Row>
+				<Col>
+					<Text>Está atendiendo el ticket número: </Text>
+					<Text style={{ fontSize: 30 }} type="danger">
+						55
+					</Text>
+				</Col>
+			</Row>
+
+			<Row>
+				<Col offset={18} span={6} align="right">
+					<Button
+						shape="round"
+						type="primary"
+						onClick={siguienteTicket}>
+						<RightOutlined /> Siguiente
+					</Button>
+				</Col>
+			</Row>
+		</>
+	);
+};
+
+export default Escritorio;
--- 03-ticket-app/ticket-app/src/pages/Ingresar.js
+++ 03-ticket-app/ticket-app/src/pages/Ingresar.js
@@ -1,51 +1,64 @@
 import React from "react";
-import { Form, Input, Button, InputNumber } from "antd";
-import { SaveOutlined} from "@ant-design/icons";
+import { Form, Input, Button, InputNumber, Typography, Divider } from "antd";
+import { SaveOutlined } from "@ant-design/icons";
+import { useHistory } from "react-router-dom";
 
 const Ingresar = () => {
+	const { Title, Text } = Typography;
+
+	const history = useHistory();
 	const onFinish = (values) => {
 		console.log("Success:", values);
+		history.push("/escritorio");
 	};
 
 	const onFinishFailed = (errorInfo) => {
 		console.log("Failed:", errorInfo);
 	};
 	return (
-		<Form
-			name="basic"
-			labelCol={{ span: 8 }}
-			wrapperCol={{ span: 8 }}
-			initialValues={{ remember: true }}
-			onFinish={onFinish}
-			onFinishFailed={onFinishFailed}>
-			<Form.Item
-				label="Nombre del agente"
-				name="agente"
-				rules={[
-					{ required: true, message: "Por favor ingrese su nombre" },
-				]}>
-				<Input />
-			</Form.Item>
+		<>
+			<Title level={2}>Ingresar</Title>
+			<Text>Ingrese su nombre y número de escritorio.</Text>
+			<Divider />
+			<Form
+				name="basic"
+				labelCol={{ span: 8 }}
+				wrapperCol={{ span: 8 }}
+				initialValues={{ remember: true }}
+				onFinish={onFinish}
+				onFinishFailed={onFinishFailed}>
+				<Form.Item
+					label="Nombre del agente"
+					name="agente"
+					rules={[
+						{
+							required: true,
+							message: "Por favor ingrese su nombre",
+						},
+					]}>
+					<Input />
+				</Form.Item>
 
-			<Form.Item
-				label="Escritorio"
-				name="escritorio"
-				rules={[
-					{
-						required: true,
-						message: "Ingrese el número de escritorio",
-					},
-				]}>
-				<InputNumber min={1} max={99} />
-			</Form.Item>
+				<Form.Item
+					label="Escritorio"
+					name="escritorio"
+					rules={[
+						{
+							required: true,
+							message: "Ingrese el número de escritorio",
+						},
+					]}>
+					<InputNumber min={1} max={99} />
+				</Form.Item>
 
-			<Form.Item wrapperCol={{ offset: 9, span: 16 }}>
-				<Button type="primary" htmlType="submit" shape="round">
-					<SaveOutlined />
-					Ingresar
-				</Button>
-			</Form.Item>
-		</Form>
+				<Form.Item wrapperCol={{ offset: 9, span: 16 }}>
+					<Button type="primary" htmlType="submit" shape="round">
+						<SaveOutlined />
+						Ingresar
+					</Button>
+				</Form.Item>
+			</Form>
+		</>
 	);
 };
 
