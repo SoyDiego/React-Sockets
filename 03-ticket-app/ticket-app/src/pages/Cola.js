e59752b13509016122e5cbb9ff60a9246d88638b
soydiego <diegofranchina@gmail.com>
2021-08-20 12:04:18 +0200

    Layout cola tickets done

--- 03-ticket-app/ticket-app/src/pages/Cola.js
+++ 03-ticket-app/ticket-app/src/pages/Cola.js
@@ -1,11 +1,103 @@
-import React from 'react'
+import React from "react";
+import { Row, Col, Typography, Divider, List, Card, Tag } from "antd";
+
+const data = [
+	{
+		ticketNo: 33,
+		escritorio: 3,
+		agente: "Fernando Herrera",
+	},
+	{
+		ticketNo: 34,
+		escritorio: 4,
+		agente: "Melissa Flores",
+	},
+	{
+		ticketNo: 35,
+		escritorio: 5,
+		agente: "Carlos Castro",
+	},
+	{
+		ticketNo: 36,
+		escritorio: 3,
+		agente: "Fernando Herrera",
+	},
+	{
+		ticketNo: 37,
+		escritorio: 3,
+		agente: "Fernando Herrera",
+	},
+	{
+		ticketNo: 38,
+		escritorio: 2,
+		agente: "Melissa Flores",
+	},
+	{
+		ticketNo: 39,
+		escritorio: 5,
+		agente: "Carlos Castro",
+	},
+];
 
 const Cola = () => {
-    return (
-        <div>
-            Cola Page
-        </div>
-    )
-}
+	const { Title, Text } = Typography;
+	return (
+		<>
+			<Title level={1}>Atendiendo al cliente</Title>
+			<Row>
+				<Col span={12}>
+					<List
+						dataSource={data.slice(0, 3)}
+						renderItem={(item) => (
+							<List.Item>
+								<Card
+									style={{ width: 300, marginTop: 16 }}
+									actions={[
+										<Tag color="volcano">
+											{item.agente}
+										</Tag>,
+										<Tag color="magenta">
+											Escritorio: {item.escritorio}
+										</Tag>,
+									]}>
+									<Title>Nro. {item.ticketNo}</Title>
+								</Card>
+							</List.Item>
+						)}
+					/>
+				</Col>
+				<Col span={12} align="center">
+					<Divider>Historial</Divider>
+					<List
+						dataSource={data.slice(3)}
+						renderItem={(item) => (
+							<List.Item>
+								<List.Item.Meta
+									title={`Ticket Nro. ${item.ticketNo}`}
+									description={
+										<>
+											<Text type="secondary">
+												En el escritorio:{" "}
+											</Text>
+											<Tag color="magenta">
+												{item.ticketNo}
+											</Tag>
+											<Text type="secondary">
+												Agente:{" "}
+											</Text>
+											<Tag color="volcano">
+												{item.agente}
+											</Tag>
+										</>
+									}
+								/>
+							</List.Item>
+						)}
+					/>
+				</Col>
+			</Row>
+		</>
+	);
+};
 
-export default Cola
+export default Cola;
