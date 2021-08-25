83c1c565593092f255d9aca5511140d8ad80d61f
soydiego <diegofranchina@gmail.com>
2021-08-25 13:17:25 +0200

    Defined routes /new, /renew, /login

---
+++ 05-chat/chat-backend/router/auth.js
@@ -0,0 +1,29 @@
+const { Router } = require("express");
+
+const router = Router();
+
+// Crear nuevos usuarios
+router.post("/new", (req, res) => {
+	res.json({
+		ok: true,
+		msg: "new",
+	});
+});
+
+// LOGIN
+router.post("/", (req, res) => {
+	res.json({
+		ok: true,
+		msg: "login",
+	});
+});
+
+// REVALIDAR TOKEN
+router.get("/renew", (req, res) => {
+	res.json({
+		ok: true,
+		msg: "renew",
+	});
+});
+
+module.exports = router;
