f6ee74369e95e2a91d7f262bcf96b9927cf9d444
soydiego <diegofranchina@gmail.com>
2021-08-28 12:08:17 +0200

    Created routes and pages with react router dom

--- 05-chat/chat-app/src/ChatApp.js
+++ 05-chat/chat-app/src/ChatApp.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import AppRouter from "./router/AppRouter";
 
 const ChatApp = () => {
-    return (
-        <div>
-            ChatApp
-        </div>
-    )
-}
+	return (
+		<div>
+			<AppRouter />
+		</div>
+	);
+};
 
-export default ChatApp
+export default ChatApp;
